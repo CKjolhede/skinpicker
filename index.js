1d2196b205046ef60d2b9cc1c85e43d7b5706454
Colm Traci <ctraci@me.com>
2024-01-02 17:54:39 -0800

    rendering

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+// 1. Fetch
+// 2. getting the data / For each
+// 3. Event Listener to for each catagories & DOM Elements
+// 4. appending the data
+
 // Global
 const URL = 'http://localhost:3000/tattoos'
-let tatObj 
+var tatObj 
+
 // DOM Selectors
-
-// Event listeners
-
-// Event handlers
-
-// Render
-function renderTattoos(tatArray) {
-    const tatGrid = document.createElement('grid')
-    tatGrid.src = tatArray.img_url
-}
-
+const traditional = document.querySelector("#traditional")
 
 // Fetch
 function fetchTattoo() {
     let data = fetch('http://localhost:3000/tattoos')
           .then( res => res.json() )
-          .then( console.log )  //render function
+          .then( data => {
+            tatObj=data
+            console.log(tatObj)} )  //render function | data => tatObj
 }
 fetchTattoo()
+
+function renderTattoos(tatArray) { 
+    tatArray.forEach(tatPic => {
+    var tatImg = document.createElement('img')
+    tatImg.src = tatPic.image
+    document.getElementById('grid-container').appendChild(tatImg)
+    tatImg.addEventListener("click" , () => {
+
+    })
+    console.log(tatPic)
+})
+}
+
+// event listeners
+traditional.addEventListener('click', () => { renderTattoos(tatObj.traditional) })
+
+
